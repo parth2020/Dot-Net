907ace44454102df242f7849fbe3bab96289d2f2
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-20 09:32:02 +0530

    Odd-Even

---
+++ odd-even.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ODD_EVEN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int a, b;
+            Console.WriteLine("Enter a Number ");
+            a = Convert.ToInt16(Console.ReadLine());
+            if (a % 2 == 0)
+                Console.WriteLine("Number is even");
+            else
+                Console.WriteLine("Number is odd");
+        }
+        
+    }
+}
