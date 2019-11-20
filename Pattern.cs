b24fbd282f76b2bffe8ebf210d9bcb39034dd899
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-20 09:21:00 +0530

    * Pattern

---
+++ Pattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, r;
+            Console.WriteLine("Enter number of Pattern");
+            r = Convert.ToInt16(Console.ReadLine());
+            for (i = 1; i <= r; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
