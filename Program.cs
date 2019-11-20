5ab312bcf76033d66aa2cc0edc2114b09367aaa1
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-20 08:36:45 +0530

    Prime Number

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,n,m,flag=0;
+            Console.WriteLine("Enter Number");
+            n = Convert.ToInt16(Console.ReadLine());
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+
+            }
+            if (flag == 1)
+                Console.WriteLine("Not Prime");
+            else
+                Console.WriteLine("Number is Prime");
+
+            Console.ReadKey();
+        }
+    }
+}
