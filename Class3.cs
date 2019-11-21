d0a8a6a807e223b5bd2c203ad52992a27404d9e9
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-21 08:52:57 +0530

    Namespace

---
+++ Class3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace shape
+{
+    namespace TwoD
+    {
+        class Area
+        {
+            public void calcArea(double r)
+            {
+                Console.WriteLine("Area of Circle is: " + (3.14 * r * r));
+            }
+
+            public void calcArea(double l,double b)
+            {
+                Console.WriteLine("Area of Rectangle is: " + (l * b));
+            }
+
+            public void calcArea(int a)
+            {
+                Console.WriteLine("Area of Square is: " + (a * a));
+            }
+        }
+    }
+
+    namespace ThreeD
+    {
+        class Volume
+        {
+
+        }
+    }
+}
