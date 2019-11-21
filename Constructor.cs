ed93f4dcde70a6b0d0f1c2afe7b0d6bba2789c3b
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-21 09:24:23 +0530

    Constructor (Example)

---
+++ Constructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstructorDemo o = new ConstructorDemo(); // defaut
+            ConstructorDemo o1 = new ConstructorDemo("Prajit","Oxford University"); //
+            ConstructorDemo o2 = new ConstructorDemo(); // static
+
+            ConstructorDemo o3 = new ConstructorDemo(o); // copy constructor
+
+            //ConstructorDemo oa = new ConstructorDemo()
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConstructorDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Constructor
+{
+    class ConstructorDemo
+    {
+        static int i = 0;
+        int a, b;
+
+        public ConstructorDemo() // default
+        {
+            Console.WriteLine("Default Constructor Called " +i++);
+            a = 2;
+            b = 3;
+        }
+
+        public ConstructorDemo(string name,string clg) //paramitarised
+        {
+            Console.WriteLine("Hello This Is " + name + " From " +clg);
+        }
+
+        static ConstructorDemo() // static
+        {
+            Console.WriteLine("Static Constructor Called " +i);
+        }
+
+        public ConstructorDemo(ConstructorDemo o) // copy
+        {
+            a = o.a;
+            b=o.b;
+
+            Console.WriteLine("Value of A is " + a +" And B is "+b);
+
+        }
+    }
+}
