68bdf7d753ebe8068745b00cbd3fb76cdda361f3
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-12-02 11:04:44 +0530

    Interface

---
+++ Interface(Main).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student o = new student();
+
+            o.getdata();
+
+            string n, p;
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+            Console.WriteLine("-------------------- Login -------------------- ");
+
+            Console.WriteLine("Enter Name: ");
+            n = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            p = Console.ReadLine();
+
+            if (o.login(n, p) == 1)
+                o.showdata();
+            else
+                Console.WriteLine("Invalid Name or Password");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Interface_Class.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interface
+{
+    class Interface_Class
+    {
+
+    }
+
+    abstract class person
+    {
+        protected string name, pass, dob;
+        protected double contact;
+
+        public abstract void getdata();
+
+    }
+
+    interface idetails
+    {
+        int login(string name, string pass);
+        void showdata();
+    }
+
+    class student : person,idetails
+    {
+        int roll;
+        string branch;
+        double result;
+
+        public override void getdata()
+        {
+            Console.WriteLine("Enter Name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Password: ");
+            pass = Console.ReadLine();
+
+            Console.WriteLine("Enter Date Of Birth: ");
+            dob = Console.ReadLine();
+
+            Console.WriteLine("Enter Contact Number: ");
+            contact = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Roll Number: ");
+            roll = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter Branch Name: ");
+            branch = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Result: ");
+            result = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public int login(string name, string pass)
+        {
+
+            if (name == this.name && pass == this.pass)
+                return 1;
+            else
+                return 0;
+ 
+        }
+
+        public void showdata()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("-------------------- Details -------------------- ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Hello " +name);
+            Console.WriteLine("Roll Number is:" +roll);
+            Console.WriteLine("Your Branch is: " +branch);
+            Console.WriteLine("Result is: " +result);
+            Console.WriteLine("Your Contact Number is " + contact);
+        }
+            
+    }
+}
+
