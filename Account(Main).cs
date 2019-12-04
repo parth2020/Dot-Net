1e13096421e5b66bc61b3dbb50fffeef906f1a25
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-12-04 09:24:57 +0530

    Account (Inheritance + Interface + Constructor)

---
+++ Account(Main).cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, address;
+            int acc_no;
+            double balance;
+            double intrate;
+            int custid;
+
+            Console.WriteLine("--------------- Enter Account Details ---------------");
+            Console.WriteLine("Enter Account Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Address:");
+            address = Console.ReadLine();
+            Console.WriteLine("Enter Customer Id");
+            custid = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Account Number");
+            acc_no = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Intrest Rate");
+            intrate = Convert.ToDouble(Console.ReadLine());
+
+            o: 
+            Console.WriteLine("Enter Balance");
+            balance = Convert.ToDouble(Console.ReadLine());
+            if (balance < 5000)
+            {
+                Console.WriteLine("!!!! Please Enter Valid Ammount !!!!");
+                goto o;
+            }
+            else
+            {
+                saving o = new saving(name, address, acc_no, balance, custid, intrate);
+                o.printdata();
+                Console.WriteLine("--------------- DEPOSIT ---------------");
+                o.deposit();
+                double r;
+                Console.WriteLine("--------------- WITHDRAW ---------------");
+                Console.WriteLine("Enter Ammount To Withdraw");
+                r = Convert.ToDouble(Console.ReadLine());
+                o.withdraw(r);
+
+                o.printdata();
+
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Account_class.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Account
+{
+    class Account_class
+    {
+        protected string name, address;
+        protected int acc_no;
+        protected double balance;
+
+        public virtual void printdata()
+        {
+        }
+    }
+
+    class saving : Account_class,itrans
+    {
+        double intrate;
+        int custid;
+
+        public saving(string name , string address, int acc_no, double balance, int custid, double intrate)
+        {
+            this.name = name;
+            this.address = address;
+            this.acc_no = acc_no;
+            this.balance = balance;
+            this.custid = custid;
+            this.intrate = intrate;
+
+        }
+
+        public override void printdata()
+        {
+            Console.WriteLine();
+            Console.WriteLine("--------------- ACCOUNT DETAILS ---------------");
+            Console.WriteLine("Hello "+name);
+            Console.WriteLine("Account Number is: " + acc_no);
+            Console.WriteLine("Balance is: " + balance);
+            Console.WriteLine("Customer Id is:" +custid);
+        }
+
+        public void deposit()
+        {
+            double d;
+            Console.WriteLine("Enter Ammount: ");
+            d = Convert.ToDouble(Console.ReadLine());
+            this.balance = this.balance + d;
+            Console.WriteLine("Your Balance is:" +balance);
+        }
+
+        public double withdraw(double r)
+        {
+            if ((balance - r) < 5000)
+            {
+                Console.WriteLine("Not Able to Withdraw...");
+                return 0;
+            }
+            else
+            {
+                this.balance = this.balance - r;
+                return balance;
+            }
+        }
+
+    }
+
+    interface itrans
+    {
+        void deposit();
+        double withdraw(double r);
+    }
+}
