aa3b3706e480d2ecf0255a4cf5dcedaa296a3dc7
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-12-05 13:28:22 +0530

    Property

---
+++ Property(Main).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Property_Class o = new Property_Class();
+
+            Console.WriteLine("Enter Your Name");
+            o.name = Console.ReadLine();
+            Console.WriteLine("Enter Roll Number");
+            o.enroll = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Marks for Subject 1");
+            o.m1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Marks for Subject 2");
+            o.m2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Marks for Subject 3");
+            o.m3 = Convert.ToInt16(Console.ReadLine());
+
+            o.show();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Property_Class.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Property
+{
+    class Property_Class
+    {
+
+        int _enroll,_m1,_m2,_m3;
+        string _name;
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int enroll
+        {
+            get { return _enroll; }
+            set { _enroll = value; }
+        }
+        
+        public int m1
+        {
+            get { return _m1; }
+            set { _m1 = value; }
+        }
+
+        public int m2
+        {
+            get { return _m2; }
+            set { _m2 = value; }
+        }
+
+        public int m3
+        {
+            get { return _m3; }
+            set { _m3 = value; }
+        }
+
+        public void show()
+        {
+            Console.WriteLine("---------------- Your Details ----------------");
+            Console.WriteLine();
+            Console.WriteLine("Hello is: "+name);
+            Console.WriteLine("Roll Number: " +enroll);
+            Console.WriteLine("Percentage: " +((m1+m2+m3)/3));
+        }
+    }
+}
