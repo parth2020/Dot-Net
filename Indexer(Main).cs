6b9aef68ea86f35a519e7aff78fb56c0024927f5
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-12-11 09:30:37 +0530

    Indexer

---
+++ Indexer(Main).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj[0] = 1;
+            obj[1] = 5.55;
+            obj[2] = "abc";
+
+            Console.WriteLine("Int Is " +obj[0] + " Double Is " +obj[1]+ " String Is " +obj[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Indexer_Class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication12
+{
+    class Class1
+    {
+        int x;
+        double y;
+        string s;
+
+        public object this[int i]
+        {
+            get
+            {
+                if (i == 0)
+                    return x;
+                else if (i == 1)
+                    return y;
+                else
+                    return s;
+            }
+
+            set
+            {
+                if (i == 0)
+                    x = Convert.ToInt16(value);
+                else if (i == 1)
+                    y = Convert.ToDouble(value);
+                else
+                    s = (string)value;
+            }
+        }
+    }
+}
