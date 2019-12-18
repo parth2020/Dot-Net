c72d8a8c3e048e8fec884e649070cbc76ff4b70a
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-12-18 09:12:21 +0530

    Indexer (Dual Array)

---
+++ Indexer(Dual Array).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indexer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Indexer_Class obj = new Indexer_Class();
+
+            int i = 0;
+
+            obj[0] = 1;
+            obj[1] = 5;
+            obj[2] = 9;
+
+            obj[3] = 2.2;
+            obj[4] = 6.6;
+            obj[5] = 10.10;
+
+            for (i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(obj[i]);
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
---
+++ Indexer(Dual Array)_Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indexer1
+{
+    class Indexer_Class
+    {
+        int[] x = new int[3];
+        double[] y = new double[3];
+
+        public object this[int i]
+        {
+            set
+            {
+                if (i <= 2)
+                    x[i] = Convert.ToInt16(value);
+                else
+                    y[i-3] = Convert.ToDouble(value);
+            }
+
+            get
+            {
+                if (i <= 2)
+                    return x[i];
+                else
+                    return y[i-3];
+            }
+        }
+    }
+}
