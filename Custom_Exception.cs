723e8d4e29d5e1f4767515ff1620879dbd8bf63e
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-12-18 09:10:21 +0530

    Custom Exception

---
+++ Custom_Exception.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Custom_Exception
+{
+    class Program
+    {
+        public void checkage(int a)
+        {
+            if (a <= 0)
+                throw new CheckAgeException("Invalid Age");
+        }
+
+        static void Main(string[] args)
+        {
+            Program o = new Program();
+
+            try
+            {
+                o.checkage(0);
+            }
+
+            catch (CheckAgeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        class CheckAgeException : ApplicationException
+        {
+            public CheckAgeException(String s)
+                : base(s)
+            {
+
+            }
+        }
+
+
+
+    }
+}
